3dfecf5f62ea9c84d2a073298a2d873d461f4c1b
PetrusStarken <petrusstarken@bitbucket.org>
2017-02-24 17:13:59 -0300

    basic html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,13 +10,17 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { RepositoriesComponent } from './repositories/repositories.component';
 import { SearchComponent } from './search/search.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     RepositoriesComponent,
-    SearchComponent
+    SearchComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
